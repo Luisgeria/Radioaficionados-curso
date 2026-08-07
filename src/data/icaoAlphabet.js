2127b29d76cdc7aecd612e4890a94605c9b0170a
luisgeria <luisgeria.ia@gmail.com>
2026-08-07 12:29:36 +0200

    Add files via upload

---
+++ src/data/icaoAlphabet.js
@@ -0,0 +1,18 @@
+export const icaoLetters = [
+  { l: 'A', w: 'Alfa' }, { l: 'B', w: 'Bravo' }, { l: 'C', w: 'Charlie' },
+  { l: 'D', w: 'Delta' }, { l: 'E', w: 'Echo' }, { l: 'F', w: 'Foxtrot' },
+  { l: 'G', w: 'Golf' }, { l: 'H', w: 'Hotel' }, { l: 'I', w: 'India' },
+  { l: 'J', w: 'Juliett' }, { l: 'K', w: 'Kilo' }, { l: 'L', w: 'Lima' },
+  { l: 'M', w: 'Mike' }, { l: 'N', w: 'November' }, { l: 'O', w: 'Oscar' },
+  { l: 'P', w: 'Papa' }, { l: 'Q', w: 'Quebec' }, { l: 'R', w: 'Romeo' },
+  { l: 'S', w: 'Sierra' }, { l: 'T', w: 'Tango' }, { l: 'U', w: 'Uniform' },
+  { l: 'V', w: 'Victor' }, { l: 'W', w: 'Whiskey' }, { l: 'X', w: 'X-ray' },
+  { l: 'Y', w: 'Yankee' }, { l: 'Z', w: 'Zulu' },
+]
+
+export const icaoNumbers = [
+  { l: '0', w: 'Zero' }, { l: '1', w: 'One' }, { l: '2', w: 'Two' },
+  { l: '3', w: 'Three' }, { l: '4', w: 'Four' }, { l: '5', w: 'Five' },
+  { l: '6', w: 'Six' }, { l: '7', w: 'Seven' }, { l: '8', w: 'Eight' },
+  { l: '9', w: 'Nine' },
+]
